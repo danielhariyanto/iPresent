9a75d96a7550300d4124d8ce17814e6ad74b1c0e
Daniel Hariyanto <danielthariyanto@gmail.com>
2021-01-16 22:59:00 +0700

    added algorithm functionality to backend

---
+++ data.py
@@ -0,0 +1,14 @@
+filler_words = [
+    'like', 
+    'I mean',
+    'you know', 
+    'so', 
+    'well', 
+    'you see', 
+]
+
+hedging_language = [
+    'kind of', 
+    'I think', 
+    'maybe',
+]
