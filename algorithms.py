f2c3519c4fec3920e1fed24a7c7945f8eef9fe2e
Daniel Hariyanto <dthariyanto@gmail.com>
2021-01-17 20:56:29 +0700

    added algorithms with backend, not finished yet

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,6 @@
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud import storage
+from google.cloud import language_v1
 
 import spacy
 
@@ -19,13 +20,24 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, LSTM, Reshape, Bidirectional
 from tensorflow.keras import backend as K
 
-from tqdm import tqdm 
-
-import gensim.downloader as api
+from tqdm import tqdm
 
 import string
 import json 
 import re
+
+import pydub
+
+import IPython.display as ipd
+
+import librosa
+import librosa.display
+from librosa import feature
+
+import soundfile as sf
+import io
+
+import operator
 
 
 def transcribe_gcs(gcs_uri):
@@ -106,25 +118,184 @@ def cadence(words):
     total_time = speech_time(words)
     pace = words_per_second(words, total_time)
 
-    return total_time, pace
+    return pace
 
 '''
-Compute the number of seconds for each word in `words`
+Common words
 '''
-def seconds_per_word(words, verbose=False):
-    word_times = {}
-    for word_info in words:
-        word = word_info.word
-        start_time = word_info.start_time.total_seconds()
-        end_time = word_info.end_time.total_seconds()
-        confidence = word_info.confidence
+def vocabulary(transcript):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(transcript)
 
-        if verbose: 
-            print(
-                f"{word}, start_time: {start_time}, end_time: {end_time}, confidence: {confidence}"
-            )
+    noun_phrases = [chunk.text for chunk in doc.noun_chunks]
+    verb_phrases = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+    adjectives = [token.lemma_ for token in doc if token.pos_ == "ADJ"]
 
-        time_of_word = round(end_time - start_time, 3)
-        word_times[word] = time_of_word
-        
-    return word_times
+    print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+    print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+    print("Adjectives:", [token.lemma_ for token in doc if token.pos_ == "ADJ"])
+
+
+"""
+Analyzing Sentiment in a String
+
+Returns dict of scores for the entire document, and a list of
+dicts representing scores for each sentence in the document. 
+
+Args:
+    text_content The text content to analyze
+"""
+def sample_analyze_sentiment(text_content):
+    client = language_v1.LanguageServiceClient()
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    
+    # Get overall sentiment of the input document
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    print(
+        u"Document sentiment magnitude: {}".format(
+            response.document_sentiment.magnitude
+        )
+    )
+    
+    return response.document_sentiment.score, response.document_sentiment.magnitude
+
+
+def create_embedding(transcript, wv):
+#     print(transcript)
+    X = []
+    found_words = []
+    transcript = clean(transcript) # remove any punctuation
+    
+    print(transcript)
+    words = transcript.split()
+    for word in words: 
+        try:
+            found_words.append(wv[word])
+        except: 
+            continue
+            
+    embedding = np.asarray(found_words)
+    mean = np.mean(embedding, axis=0)
+    mean = mean.tolist()
+
+    if type(mean) == list: 
+        X.append(mean)
+
+    X = np.array(X)
+    return X
+
+
+def ai_impression(transcript, wv):
+    wv = load_wv()
+    embedding = create_embedding(transcript, wv)
+
+    model_filepath = './static/models/ted_analysis_model'
+    model = tf.keras.models.load_model(model_filepath)
+    
+    pred = model.predict(embedding)[0]
+    cols = ['Beautiful', 'Confusing', 'Courageous', 'Funny', 'Informative', 'Ingenious', 'Inspiring', 'Longwinded', 'Unconvincing', 'Fascinating', 'Jaw-dropping', 'Persuasive', 'OK', 'Obnoxious']
+
+    ted_dict = {}
+    for val, col in zip(pred, cols):
+        ted_dict[col] = val
+    
+    word_result = max(stats.iteritems(), key=operator.itemgetter(1))[0]
+
+    return word_result
+
+
+'''
+This function reads in a .wav file. 
+
+Set cloud to true if data file path is from the cloud.
+'''
+def read_wav(bucket, file_name):
+    # read a blob
+    
+    blob = bucket.blob(file_name)
+    file_as_string = blob.download_as_string()
+
+    # convert the string to bytes and then finally to audio samples as floats 
+    # and the audio sample rate
+    data, sample_rate = sf.read(io.BytesIO(file_as_string))
+
+    # print(len(data.shape))
+    if len(data.shape) == 1: 
+        left_channel = data[:]  # I assume the left channel is column zero
+    elif len(data.shape) == 2: 
+        left_channel = data[:, 0]  # I assume the left channel is column zero
+
+    # enable play button in datalab notebook
+    aud = ipd.Audio(left_channel, rate=sample_rate)
+    
+    return aud, data, sample_rate
+
+'''
+Get spectrogram for each audio file. 
+'''
+def get_spectrogram(y, sr):
+    spectrogram = librosa.feature.melspectrogram(y, sr)
+    return spectrogram
+
+# Intensity analysis (0 = neutral, 1 = strong/passionate)
+def get_intensity_analysis(emotion_intensity):
+    emotions = []
+    total = 0
+    sums = {0: 0, 1: 0}
+    for emotion in emotion_intensity: 
+        if emotion[0] >= 0.5:
+            emotions.append('Passionate')
+            sums[1] += 1
+        else:         
+            emotions.append('Neutral')
+            sums[0] += 1
+        total += 1
+    percentages = {'Neutral': sums[0] / total, 'Passionate': sums[1] / total}
+    return emotions, percentages
+
+def perform_audio_analysis(bucket_name, filename):
+    audio_model_filepath = './static/models/audio_analysis_model'
+    emotion_intensity_model_filepath = './static/models/emotion_intensity_model'
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(bucket_name)
+    aud, y, sr = read_wav(bucket, filename)
+    
+    # Convert into spectrogram
+    spectrogram = get_spectrogram(y[:, 0], sr)
+    
+    # Load models
+    audio_model = tf.keras.models.load_model(audio_model_filepath)
+    emotion_intensity_model = tf.keras.models.load_model(emotion_intensity_model_filepath)
+    
+    # Break into time-based chunks, every ~3 seconds 
+    num_chunks = spectrogram.shape[-1] // 124
+    sound_chunks = []
+    for i in range(num_chunks): 
+        chunk = spectrogram[:, i * 124 : (i+1) * 124]
+        sound_chunks.append(chunk)
+    sound_chunks = np.array(sound_chunks)
+    sound_chunks = np.expand_dims(sound_chunks, axis=-1)
+    
+    # Make predictions
+    audio_analysis = audio_model.predict(sound_chunks)
+    emotion_intensity = emotion_intensity_model.predict(sound_chunks)
+
+    emotions, emotion_percentages = get_intensity_analysis(emotion_intensity)
+    
+    # Classifies audio based on neutrality vs. strength (aka passion/intensity)
+    emote_stats = (emotions, emotion_percentages)
+    return emote_stats
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from google.cloud import storage
 import algorithms
 from data import filler_words, hedging_language
+import gensim.downloader as api
 
 
 app = Flask(__name__)
@@ -42,17 +43,45 @@ def results():
     filler_phrase_counts, filler_all_counts = algorithms.count_phrases(transcript, filler_words)
     hedging_phrase_counts, hedging_all_counts = algorithms.count_phrases(transcript, hedging_language)
     #cadence
-    total_time, pace = algorithms.cadence(words)
-    #seconds / word
-    word_times = algorithms.seconds_per_word(words, verbose=True)
+    wpm = algorithms.cadence(words)
+    #common words
+    #common_words = algorithms.vocabulary(transcript)
+    common_words = {
+        "first_word": "first",
+        "first_word_count": 8,
+        "second_word": "second",
+        "second_word_count": 5,
+        "third_word": "third",
+        "third_word_count": 3,
+        "fourth_word": "fourth",
+        "fourth_word_count": 2
+    }
+    first_word = common_words["first_word"]
+    first_word_count = common_words["first_word_count"]
+    second_word = common_words["second_word"]
+    second_word_count = common_words["second_word_count"]
+    third_word = common_words["third_word"]
+    third_word_count = common_words["third_word_count"]
+    fourth_word = common_words["fourth_word"]
+    fourth_word_count = common_words["fourth_word_count"]
+    #sentiment analysis
+    sentiment_score, sentiment_magnitude = algorithms.sample_analyze_sentiment(transcript)
+    #AI impression (JSON)
+    #ai_impression = algorithms.ai_impression(transcript, wv)
+    ai_impression = "Beautiful"
+    #expressiveness
+    #emote_stats = algorithms.perform_audio_analysis(bucket_name, blob_name)
+    emote_stats = {'Neutral': 0.7777777777777778, 'Passionate': 0.2222222222222222}
 
 
     ### DELETE AUDIO FILE ###
     delete_blob(bucket_name, blob_name)
 
-    
-    return render_template("metrics.html", clarity=clarity, phrase_counts=filler_all_counts+hedging_all_counts, pace=pace)
+    return render_template("metrics.html", clarity=clarity, brevity=filler_all_counts+hedging_all_counts, wpm=wpm*60, common_words=common_words, first_word=first_word, first_word_count=first_word_count, second_word=second_word, second_word_count=second_word_count, third_word=third_word, third_word_count=third_word_count, fourth_word=fourth_word, fourth_word_count=fourth_word_count, sentiment_score=sentiment_score, sentiment_magnitude=sentiment_magnitude, ai_impression=ai_impression, expressiveness=emote_stats["Passionate"])
 
+def load_model():
+    global wv
+    wv = api.load('word2vec-google-news-300')
 
 def delete_blob(bucket_name, blob_name):
     """Deletes a blob from the bucket after algorithms"""
@@ -66,4 +95,5 @@ def delete_blob(bucket_name, blob_name):
 
 
 if __name__ == "__main__":
+    #load_model()
     app.run(debug=True)
